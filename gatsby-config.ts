53a29c0a740106d0de710454ea39002496021199
liz <liz@inappstory.com>
2023-11-15 16:12:44 +0300

    new path resolving mechanism, need to check on mac

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,19 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-import path from 'path'
+import path from "path";
+
+function resolvePath(inputPath: string): string {
+  return inputPath.split(path.sep).reduce((reducer, value) => {
+    reducer += path.sep;
+    return reducer.concat(value);
+  });
+}
 
 const paths = {
-  images: __dirname + path.sep + `static` + path.sep + `images` + path.sep,
-}
+  // images: __dirname + path.sep + `static` + path.sep + `images` + path.sep,
+  images: resolvePath(`${__dirname}\\static\\images\\`),
+};
 
 module.exports = {
   plugins: [
