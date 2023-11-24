2937b957967183a8cb41eee6713fc71e5db56762
Liza <liz@inappstory.com>
2023-11-24 12:03:14 +0300

    safety commit

--- src/components/index/review-card.tsx
+++ src/components/index/review-card.tsx
@@ -17,14 +17,14 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({
 }) => {
   return (
     <div className="review-card">
-      <p className="review-card__text">{reviewText}</p>
+      <p className="review-card__text text">{reviewText}</p>
       <div className="review-card__reviewer">
         <img
           className="review-card__avatar"
           src={avatar}
           alt="partner avatar"
         />
-        <p className="review-card__reviewer-info">{reviewerInfo}</p>
+        <p className="review-card__reviewer-info text">{reviewerInfo}</p>
         <img
           className="review-card__brand-image"
           src={brandImage}
